9cf92cd6651b91cdcc268f32db6ca5a86d01b8ce
mahedjaved <mahed95@gmail.com>
2024-11-17 15:26:47 +0000

    new testing address

---
+++ src/hello_world.py
@@ -0,0 +1,8 @@
+from locust import HttpUser, task, between
+
+class HelloWorld(HttpUser):
+    wait_time = between(5, 15)
+
+    @task
+    def test(self):
+        self.client.get("https://dns.google")
